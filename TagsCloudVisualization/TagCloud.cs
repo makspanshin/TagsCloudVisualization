fd7095dfa9b4fd71953cbc06598a95a599783761
makspanshin <makspanshin97@gmail.com>
2023-02-06 23:33:57 +0500

    Create TagCloud.cs

---
+++ TagsCloudVisualization/TagCloud.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TagsCloudVisualization.Interfaces;
+
+namespace TagsCloudVisualization
+{
+    public  class TagCloud
+    {
+        private readonly IReaderWords readerWords;
+        private readonly IPreprocessorWords preprocessorWords;
+        private readonly ICloudLayouter cloudLayouter;
+        private readonly IPainter painter;
+        private readonly ISpiral spiral;
+        //var centrePoint = new Point(750, 750);
+
+        public TagCloud(IReaderWords readerWords, IPreprocessorWords preprocessorWords, ICloudLayouter cloudLayouter, IPainter painter, ISpiral spiral)
+        {
+            this.readerWords = readerWords;
+            this.preprocessorWords = preprocessorWords;
+            this.cloudLayouter = cloudLayouter;
+            this.painter = painter;
+            this.spiral = spiral;
+        }
+
+
+    }
+}

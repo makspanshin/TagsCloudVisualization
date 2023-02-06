b0d828e8c4ecc91be54cf5ffd1559549d43582b7
makspanshin <makspanshin97@gmail.com>
2023-02-06 23:33:46 +0500

    Create TagCloudSetting.cs

---
+++ DebugProject/TagCloudSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TagsCloudVisualization;
+
+namespace DebugProject
+{
+    internal class TagCloudSetting : ITagCloudSettings
+    {
+        public int ImageHeight { get; set; }
+        public int ImageWidth { get; set; }
+        public int FontSize { get; set; }
+        public string BackgroundColor { get; set; }
+    }
+}

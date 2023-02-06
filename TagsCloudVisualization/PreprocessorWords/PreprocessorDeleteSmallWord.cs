4f46299b65e3dbeaa9590ae4c989eec0a7d3813e
makspanshin <makspanshin97@gmail.com>
2023-02-06 21:03:14 +0500

    Create PreprocessorDeleteSmallWord.cs

---
+++ TagsCloudVisualization/PreprocessorWords/PreprocessorDeleteSmallWord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TagsCloudVisualization.Interfaces;
+
+namespace TagsCloudVisualization.PreprocessorWords
+{
+    public class PreprocessorDeleteSmallWord : IPreprocessor
+    {
+        private HashSet<string> ignoreWords = new HashSet<string>()
+        {
+            "в",
+            "ты",
+            "на",
+            "он",
+            "без",
+            "до",
+            "для",
+            "за",
+            "через",
+            "над",
+            "по",
+            "из",
+            "у",
+        };
+
+        public PreprocessorDeleteSmallWord()
+        {
+        }
+
+        public IEnumerable<string> Correct(IEnumerable<string> words)
+        {
+            List<string> correctedWords = new List<string>();
+            foreach (var item in words)
+            {
+                if (!ignoreWords.Contains(item))
+                    correctedWords.Add(item);
+            }
+            return correctedWords;
+        }
+    }
+}

c3070d4c0a58b3410459bcfe6b62752391867b9c
makspanshin <makspanshin97@gmail.com>
2023-02-06 23:33:49 +0500

    Create ITagCloudSettings.cs

---
+++ TagsCloudVisualization/ITagCloudSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TagsCloudVisualization
+{
+    public interface ITagCloudSettings
+    {
+        public int ImageHeight { get; set; }
+        public int ImageWidth { get; set; }
+        public int FontSize { get; set; }
+        public string BackgroundColor { get; set; }
+    }
+}

e20478d94ee3368b9da2877764849a57b1372b7b
makspanshin <makspanshin97@gmail.com>
2023-02-09 10:32:29 +0500

    Завернул функционал связанный с отрисовкой, в отдельный класс

---
+++ TagsCloudVisualization/Interfaces/IVisualizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TagsCloudVisualization.Interfaces
+{
+    public interface IVisualizer
+    {
+         Bitmap Visualize(IEnumerable<string> words);
+    }
+}
---
+++ TagsCloudVisualization/Visualizer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using TagsCloudVisualization.Interfaces;
+
+namespace TagsCloudVisualization
+{
+    public class Visualizer : IVisualizer
+    {
+        private readonly ICloudLayouter cloudLayouter;
+        private readonly IPainter painter;
+
+        public Visualizer(ICloudLayouter cloudLayouter, IPainter painter)
+        {
+            this.cloudLayouter = cloudLayouter;
+            this.painter = painter;
+        }
+
+        public Bitmap Visualize(IEnumerable<string> words)
+        {
+            MeasurerString measurerString = new MeasurerString();
+            var rectangles = new List<Rectangle>();
+            foreach (var item in words)
+            {
+                rectangles.Add(cloudLayouter.PutNextRectangle(measurerString.MeasureString(item, 15)));
+            }
+
+            return painter.CreateImage(rectangles, words.ToList());
+        }
+    }
+}

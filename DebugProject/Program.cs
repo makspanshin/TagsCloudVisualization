6f4972f79ddc604f8861826ed3fcd2833503b8c4
makspanshin <makspanshin97@gmail.com>
2023-02-09 11:07:30 +0500

    Add DI

--- DebugProject/Program.cs
+++ DebugProject/Program.cs
@@ -1,33 +1,43 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using TagsCloudVisualization;
+using TagsCloudVisualization.Interfaces;
 using TagsCloudVisualization.PreprocessorWords;
 
 namespace DebugProject
 {
     class Program
     {
+        private static IContainer Container { get; set; }
         static void Main()
         {
-            TagCloudSetting tagCloudSetting = new TagCloudSetting();
+            var tagCloudSetting = new TagCloudSetting();
             tagCloudSetting.ImageWidth = 1500;
             tagCloudSetting.ImageHeight = 1500;
             tagCloudSetting.PathToWords = "Words.txt";
 
-            var spiral = new ArchimedesSpiral(tagCloudSetting);
-            var readerWord = new  ReaderWord(tagCloudSetting);
-            var preprocessorWords = new PreprocessorWords();
-            preprocessorWords.AddPreprocessor(new PreprocessorToLowercase());
-            preprocessorWords.AddPreprocessor(new PreprocessorDeleteSmallWord());
-            var painter = new PainterOfRectangles(tagCloudSetting);
-            var circularCloudLayouter = new CircularCloudLayouter(tagCloudSetting, spiral);
-            var visualizer = new Visualizer(circularCloudLayouter,painter);
-
-            TagCloud tagCloud = new TagCloud(readerWord, preprocessorWords, visualizer);
-
-            tagCloud.Draw();
+            Container = Configure(tagCloudSetting);
+            Container.Resolve<ITagCloud>().Draw();
         }
+
+        private static IContainer Configure(ITagCloudSettings tagCloudSettings)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ArchimedesSpiral>().As<ISpiral>();
+            builder.RegisterType<ReaderWord>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<PreprocessorDeleteSmallWord>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<PreprocessorToLowercase>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<PreprocessorWords>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<PainterOfRectangles>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<CircularCloudLayouter>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<Visualizer>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<TagCloud>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterInstance(tagCloudSettings).AsImplementedInterfaces().SingleInstance();
+            return builder.Build();
+        }
+
     }
 }

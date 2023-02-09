b3b97c54ef81d7ca3e9f404c8fb8603fbbac66ac
makspanshin <makspanshin97@gmail.com>
2023-02-09 10:33:29 +0500

    Create MeasurerString.cs
    Класс определяет размеры строки в пикселях

---
+++ TagsCloudVisualization/MeasurerString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TagsCloudVisualization
+{
+    internal class MeasurerString
+    {
+        public MeasurerString()
+        {
+        }
+
+        public Size MeasureString(string word, int sizeFont)
+        {
+            using Font font = new Font("Times", sizeFont);
+
+            return new Size((int)font.Size * word.Length, font.Height);
+        }
+    }
+}

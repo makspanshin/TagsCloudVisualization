7d7050cfd18a1e0170f831a887f4ffd1abcbb3e3
makspanshin <makspanshin97@gmail.com>
2023-02-10 18:47:26 +0500

    Create ColorConvector.cs

---
+++ TagsCloudVisualization/Helpers/ColorConvector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TagsCloudVisualization.Helpers
+{
+    public static class ColorConvector
+    {
+
+        public static Color FromHex(string hex)
+        {
+            hex = hex.Replace("#", string.Empty);
+            var a = (byte)Convert.ToUInt32(hex.Substring(0, 2), 16);
+            var r = (byte)Convert.ToUInt32(hex.Substring(2, 2), 16);
+            var g = (byte)Convert.ToUInt32(hex.Substring(4, 2), 16);
+            var b = (byte)Convert.ToUInt32(hex.Substring(6, 2), 16);
+            return Color.FromArgb(a, r, g, b);
+        }
+    }
+
+}
